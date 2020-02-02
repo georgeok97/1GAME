4090a59b7f58529122249b737c0647b38e0f3561
georgeok97 <georgeok97@yandex.ru>
2020-02-02 13:29:39 +1000

    143

--- main.py
+++ main.py
@@ -1,59 +1,58 @@
 import pygame
 
 level = [
-    '------------------------------------------------------------------------------------------------------------------------------------------------------------',
-    '-                                                                                                                                                          -',
-    '-                                       -   -   -   -----                                                                                                  -',
-    '-                                     - -   -   -       -                                                                                                  -',
-    '-                                       -   -----   -----                                                                                                  -',
-    '-                                       -       -       -                                                                                                  -',
-    '-                                       -       -   -----                                                                                                  -',
-    '-                                                                                                                                                          -',
-    '-                                                                                   -----------                                                            -',
-    '-                                                                                 --           --                                                          -',
-    '-                                                                                 -    -- --    -                                                          -',
-    '-                                                                                 -             -                                                          -',
-    '-                                                                                 -    -----    -                                                          -',
-    '-                                                                                 --           --                                                          -',
-    '-                                                                                   -----------                                                            -',
-    '-                                                                                                                                                          -',
-    '-                                                                                                                                                          -',
-    '-                                                                                                                                                          -',
-    '-                                                                                                                                                          -',
-    '-                                                                                                                                                          -',
-    '------------------------------------------------------------------------------------------------------------------------------------------------------------'
-]
+    # '-' * int(WIN_WIDTH / BRICK_WIDTH),
+    '----------------------------------------------------------------------------------------------------------------------------------',
+    '-                           -                          --                        ---                                  -          -',
+    '-                                                      --                                                       -                -',
+    '-                                                                                                                                -',
+    '-            --                                                   -                                                              -',
+    '-                                                                                                                                -',
+    '--                                                                -                                                              -',
+    '-                                                                                   ---                                          -',
+    '-                                       --                                          ---                                     ---  -',
+    '-                                                                                                                                -',
+    '-                                                                                                                                -',
+    '-                                                                                 -                                              -',
+    '-                                                     -                                                                          -',
+    '-   --------                                          -                                                                          -',
+    '-                                                                                                                  -             -',
+    '-                                                                  --                                              -             -',
+    '-                   --                                                                                                           -',
+    '-                                                                             -                                  --              -',
+    '-                                   -          -                             -                                  ---              -',
+    '-                                   -                                       -                           -                        -',
+    '----------------------------------------------------------------------------------------------------------------------------------']
 
 WIN_WIDTH, WIN_HEIGHT = 780, 630
 BG_COLOR = (192, 192, 192)
-# BRICK_WIDTH = BRICK_HEIGHT = 30
-BRICK_WIDTH, BRICK_HEIGHT = 30, 30
+BRICK_WIDTH = BRICK_HEIGHT = 30
 BRICK_COLOR = (0, 128, 0)
 BRICK_COLOR_2 = (255, 128, 0)
 FPS = 60
 clock = pygame.time.Clock()
-x1, y1 = WIN_WIDTH // 2, WIN_HEIGHT // 2
 PLAYER_SIZE = 40
 BG_SPEED = 3
 dx = 0
 PLAYER_SPEED = 3
 penalty = 0.0
-BTN_W, BYN_H = 220, 60
+BTN_W, BTN_H = 220, 60
 GOLD = (255, 215, 0)
-BLUE = (0, 0, 255)
+BLUE = (0, 0, 255, 255)
 RED = (255, 0, 0)
+WHITE = (255, 255, 255)
 
 pygame.init()
 pygame.display.set_caption('первая игра')
 screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 
-player = pygame.Surface((PLAYER_SIZE, PLAYER_SIZE))
-player.set_colorkey((0, 0, 0))
+player = pygame.Surface((PLAYER_SIZE, PLAYER_SIZE), pygame.SRCALPHA)
+# player.set_colorkey((0, 0, 0))
 
 
-def face():
-    pygame.draw.circle(player, (0, 0, 250), (PLAYER_SIZE // 2, PLAYER_SIZE // 2), PLAYER_SIZE // 2)
-    pygame.draw.circle(player,  GOLD, (12, 15), 4)
+def face(color):
+    pygame.draw.circle(player, color, (PLAYER_SIZE // 2, PLAYER_SIZE // 2), PLAYER_SIZE // 2)
+    pygame.draw.circle(player, GOLD, (12, 15), 4)
     pygame.draw.circle(player, GOLD, (28, 15), 4)
     pygame.draw.arc(player, GOLD, (8, 12, 24, 20), 3.6, 6.0, 3)
 
@@ -61,12 +60,15 @@ def face():
 player_rect = player.get_rect(center=(WIN_WIDTH // 2, WIN_HEIGHT // 2))
 
 text = pygame.font.SysFont('Arial', 22, True, False)
-text_xy = ((WIN_WIDTH - text.size(f'PENALTY POINTS: {round(penalty, 1)}')[0]) // 2, 30)
+text_xу = ((WIN_WIDTH - text.size(f'Штрафных очков: {round(penalty, 1)}')[0]) // 2, 30)
 
-btn = pygame.Surface((BTN_W, BYN_H))
-text1 = 'TRY AGAIN?'
+btn = pygame.Surface((BTN_W, BTN_H))
+btn.fill(BLUE)
+text1 = 'ИГРАТЬ СНОВА ?'
 text1_xy = text.size(text1)
 
+color = BLUE
+face(color)
 run = True
 while run:
     for e in pygame.event.get():
@@ -84,8 +86,11 @@ while run:
         player_rect.y += PLAYER_SPEED
 
     screen.fill(BG_COLOR)
+    if color != BLUE:
+        color = BLUE
+        face(color)
 
-    if dx > -WIN_WIDTH * 5:
+    if dx > -WIN_WIDTH * 4:
         dx -= BG_SPEED
     else:
         if player_rect.x < WIN_WIDTH - PLAYER_SIZE:
@@ -96,18 +101,30 @@ while run:
     for row in level:
         for col in row:
             if col == '-':
-                # screen.blit(brick, (x, y))
                 brick = pygame.draw.rect(screen, BRICK_COLOR, [x, y, BRICK_WIDTH, BRICK_HEIGHT])
-                pygame.draw.rect(screen, (BRICK_COLOR_2), [x, y, BRICK_WIDTH, BRICK_HEIGHT], 2)
+                pygame.draw.rect(screen, BRICK_COLOR_2, [x, y, BRICK_WIDTH, BRICK_HEIGHT], 2)
                 if brick.colliderect(player_rect):
+                    if color == BLUE:
+                        color = RED
+                        face(color)
                     penalty += 0.1
             x += BRICK_WIDTH
         y += BRICK_HEIGHT
         x = dx
 
-    screen.blit(player, player_rect)
-    screen.blit(
-        text.render(f'PENALTY POINTS: {round(penalty, 1)}', True, RED, None), text_xy)
+    if player_rect.x < WIN_WIDTH - PLAYER_SIZE:
+        screen.blit(player, player_rect)
+        screen.blit(
+            text.render(f'Штрафных очков: {round(penalty, 1)}', True, RED, None), text_xу)  # изменил
+    else:
+        screen.blit(btn, ((WIN_WIDTH - BTN_W) // 2, WIN_HEIGHT // 2))
+        screen.blit(
+            text.render(text1, True, WHITE, None),
+            ((BTN_W - text1_xy[0] // 2, (BTN_H - text1_xy[1]) // 2)))
+        screen.blit(
+            text.render(f'Штрафных очков: {round(penalty, 1)}', True, RED, None),
+            ((WIN_WIDTH - text.size(f'Штрафных очков: {round(penalty, 1)}')[0]) // 2,
+             WIN_HEIGHT // 2 - BTN_H))
 
     pygame.display.set_caption(f' FPS: {round(clock.get_fps(), 2)}')
     pygame.display.update()
